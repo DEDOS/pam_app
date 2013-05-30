1137fdf9866d1908d04b493f1dd0cd5de91770bb
Dedos <raulrmh24@gmail.com>
2013-05-29 19:59:51 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PamApp::Application.config.secret_token = '973f6f9928ab41a482c0c20974c756c476ada3af72eb892fb1c25bb988c115db70b98dc433b59a3ff2a6e71848c1eabe2dc64959342d8e7abf72762e0e5194a3'
